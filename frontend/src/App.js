8ad7ca0f7098f09682ff158bde6ad75f2dc20658
Mohit <mohitkhatri4272@gmail.com>
2023-06-02 18:41:07 +0530

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import React from 'react';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
    return (
-      <>
+      <BrowserRouter>
          <Header />
          <main className='py-3'>
             <Container>
-               <h1>We</h1>
+               <Routes>
+                  <Route path='/' element={<HomeScreen />} exact />
+                  <Route path='/product/:id' element={<ProductScreen />} />
+               </Routes>
             </Container>
          </main>
          <Footer />
-      </>
+      </BrowserRouter>
    );
 };
 

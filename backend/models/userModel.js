8dbcb832c145a12582506012f667d57aa09f6dbe
Mohit <mohitkhatri4272@gmail.com>
2023-07-02 13:43:25 +0530

    Back End User Authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
    {
@@ -10,6 +11,7 @@ const userSchema = mongoose.Schema(
          type: String,
          required: true,
          unique: true,
+         lowercase: true,
       },
       password: {
          type: String,
@@ -25,6 +27,18 @@ const userSchema = mongoose.Schema(
       timestamps: true,
    }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre('save', async function (next) {
+   if (!this.isModified('password')) {
+      next();
+   }
+   const salt = await bcrypt.genSalt(10);
+   this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
